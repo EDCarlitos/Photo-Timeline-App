08ad267f24f38ad593b10afaec03898818fe6063
EDCarlitos <juancarlosuchdzib@gmail.com>
2026-02-23 08:56:14 -0500

    Feat(TakePhoto) TakePhotoButton.kt UI done

--- app/src/main/java/com/example/proyecto_2/ui/theme/Camera/CameraControls.kt
+++ app/src/main/java/com/example/proyecto_2/ui/theme/Camera/CameraControls.kt
@@ -1,26 +1,10 @@
 package com.example.proyecto_2.ui.theme.Camera
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FlipCameraAndroid
-import androidx.compose.material.icons.filled.FlipCameraIos
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.proyecto_2.R
 
 @Composable
 fun CameraBottonControls(modifier: Modifier = Modifier) {
@@ -29,7 +13,7 @@ fun CameraBottonControls(modifier: Modifier = Modifier) {
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         FlipCameraButton()
-        FlipCameraButton()
+        TakePhotoButton()
         FlipCameraButton()
 
     }
---
+++ app/src/main/java/com/example/proyecto_2/ui/theme/Camera/TakePhotoButton.kt
@@ -0,0 +1,48 @@
+package com.example.proyecto_2.ui.theme.Camera
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.proyecto_2.viewModel.Camara.CameraViewModel
+
+@Composable
+fun TakePhotoButton(
+    viewModel: CameraViewModel = viewModel(),
+    modifier: Modifier = Modifier) {
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .size(80.dp)
+            .clip(CircleShape)
+            .border(2.dp,Color.White, CircleShape)
+            .background(Color.Transparent)
+    ) {
+
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(Color.White)
+        )
+    }
+
+
+
+}
+
+@Preview
+@Composable
+private fun prev() {
+    TakePhotoButton()
+}
